cf215770ed30b64f4a5e71c832b9a4a7d1e84575
Tarbolin69 <tarbolin69@gmail.com>
2025-05-16 14:52:45 -0300

    el primero

---
+++ src/test/java/com/stock/koto/KotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.koto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
